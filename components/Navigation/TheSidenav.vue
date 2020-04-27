868be002e2b90ab66b93eba8458b5af71f7fc037
Niño Estole <antonino.estole@jpj.com>
2020-04-27 21:16:14 +0800

    SSG dynamic handling of data from CMS Admin, fix side nav links

--- components/Navigation/TheSidenav.vue
+++ components/Navigation/TheSidenav.vue
@@ -11,9 +11,9 @@
       <ul
         class="nav-list"
         @click="$emit('close')">
-        <li class="nav-item"><nuxt-link to="/posts">Blog</nuxt-link></li>
+        <li class="nav-item"><nuxt-link to="/posts">Campaigns</nuxt-link></li>
         <li class="nav-item"><nuxt-link to="/about">About</nuxt-link></li>
-        <li class="nav-item"><nuxt-link to="/admin">Admin</nuxt-link></li>
+        <li class="nav-item"><a href="/admin/#/" target="_blank">Admin</a></li>
       </ul>
     </div>
   </transition>
