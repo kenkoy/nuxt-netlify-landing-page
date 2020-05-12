47db6492e88e4f9934d8a5bbb42b88eb0652d351
Niño Estole <antonino.estole@jpj.com>
2020-05-12 21:33:46 +0800

    DEV-23327: Integrate CMS collection with Template 3

---
+++ components/VJLP3/GameSlider.vue
@@ -0,0 +1,27 @@
+<template>
+    <section id="bf-game-slider">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="slider">
+                    <a href="#" class="hvr-shrink">
+                        <div class="bf-game-icon-1"></div>
+                    </a>
+                        <a href="#" class="hvr-shrink">
+                        <div class="bf-game-icon-2"></div>
+                    </a>
+                        <a href="#" class="hvr-shrink">
+                        <div class="bf-game-icon-3"></div>
+                    </a>
+                        <a href="#" class="hvr-shrink">
+                        <div class="bf-game-icon-4"></div>
+                    </a>
+                        <a href="#" class="hvr-shrink">
+                        <div class="bf-game-icon-5"></div>
+                    </a>
+                    </div>
+                </div>
+            </div>		 
+        </div>
+	</section>
+</template>
---
+++ pages/vjlp3/_slug/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <Header 
+      :header_login_button = "post.attributes.header_login_button"
+      :header_join_button = "post.attributes.header_join_button"/>
+    <PromotionalBanner 
+      :promo_title = "post.attributes.promo_title"
+      :promo_description = "post.attributes.promo_description"
+      :promo_join_button = "post.attributes.promo_join_button"/>
+    <GameSlider />
+    <MainContent :promo_content = "post.html"/>
+  </div>
+</template>
+
+<script>
+import Header from '~/components/VJLP3/Header.vue'
+import PromotionalBanner from '~/components/VJLP3/PromotionalBanner.vue'
+import GameSlider from '~/components/VJLP3/GameSlider.vue'
+import MainContent from '~/components/VJLP3/MainContent.vue'
+
+export default {
+    layout: 'vjlp3',
+
+    components: {
+      Header,
+      PromotionalBanner,
+      GameSlider,
+      MainContent
+    },
+
+    async asyncData ({ params }) {
+      try {
+        const post = await import('~/assets/content/landing-page/'+ params.slug +'.md')
+        return {
+          post
+        }
+      } catch (error) {
+        return false
+      }
+    }
+}
+</script>
+
+<style scoped> 
+  #bf-banner { background: linear-gradient(rgba(0, 0, 0, 0.15), rgba(0, 0, 0, 0.15)), url("~assets/images/bg.jpg") center no-repeat; background-size: cover; }
+  .bf-game-icon-1 { background: url("~assets/images/icon_1.png") center no-repeat;  background-size: contain; }
+  .bf-game-icon-2 { background: url("~assets/images/icon_2.png") center no-repeat;  background-size: contain; }
+  .bf-game-icon-3 { background: url("~assets/images/icon_3.png") center no-repeat;  background-size: contain; }
+  .bf-game-icon-4 { background: url("~assets/images/icon_4.png") center no-repeat;  background-size: contain; }
+  .bf-game-icon-5 { background: url("~assets/images/icon_5.png") center no-repeat;  background-size: contain; }
+  #strip .small-terms { display: block; } #strip { background:none; }
+  #bf-banner-img-2 { background: url("~assets/images/banner-2.png") bottom center no-repeat; background-size: contain; }
+  @media only screen and (max-width: 767px) {
+    #bf-banner-img-2 { background: url("~assets/images/banner-2-m.png") bottom center no-repeat; background-size: contain; }
+  }
+</style>
+
