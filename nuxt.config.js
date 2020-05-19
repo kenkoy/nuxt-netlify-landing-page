16c0f2a53bd31153d4f8cfd25ac7f0dcfbf196fc
Dan Weather <spamdrap@gmail.com>
2020-05-19 23:25:09 +0800

    Moved back the google fonts in the head

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,7 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Open+Sans', body: true },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Open+Sans' },
     ]
   },
   /*
