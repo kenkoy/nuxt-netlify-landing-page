ef0dc72b13037de10624b79049a087728b15a4c5
Dan Weather <spamdrap@gmail.com>
2020-05-19 23:22:16 +0800

    moved the scripts from head to body

--- layouts/vjlp3.vue
+++ layouts/vjlp3.vue
@@ -24,13 +24,13 @@
           { src: 'https://files.vjpromo.com/bannerflow/lp-core/js/jquery-1.11.3.min.js' },
           { defer: true, src: 'https://files.vjpromo.com/bannerflow/lp3/brand_vj/js/before.js' },
           /* Optional JavaScript */
-          { defer: true, src: 'https://files.vjpromo.com/bannerflow/lp3/brand_vj/js/after.js' },
-          { type: 'text/javascript', src: 'https://files.vjpromo.com/bannerflow/lp-core/brand_vj/js/form.js' },
-          { type: 'text/javascript', src: 'https://files.vjpromo.com/bannerflow/lp-core/brand_vj/js/footer.js' },
-          { defer: true, src: 'https://files.vjpromo.com/bannerflow/lp-core/js/popper.min.js' },
-          { defer: true, src: 'https://files.vjpromo.com/bannerflow/lp-core/js/bootstrap.min.js' },
-          { defer: true, src: 'https://files.vjpromo.com/bannerflow/lp-core/js/slick.min.js' },
-          { defer: true, src: 'https://files.vjpromo.com/bannerflow/lp-core/js/jquery.flexverticalcenter.js' }
+          { defer: true, src: 'https://files.vjpromo.com/bannerflow/lp3/brand_vj/js/after.js', body: true },
+          { type: 'text/javascript', src: 'https://files.vjpromo.com/bannerflow/lp-core/brand_vj/js/form.js', body: true },
+          { type: 'text/javascript', src: 'https://files.vjpromo.com/bannerflow/lp-core/brand_vj/js/footer.js', body: true },
+          { defer: true, src: 'https://files.vjpromo.com/bannerflow/lp-core/js/popper.min.js', body: true },
+          { defer: true, src: 'https://files.vjpromo.com/bannerflow/lp-core/js/bootstrap.min.js', body: true },
+          { defer: true, src: 'https://files.vjpromo.com/bannerflow/lp-core/js/slick.min.js', body: true },
+          { defer: true, src: 'https://files.vjpromo.com/bannerflow/lp-core/js/jquery.flexverticalcenter.js', body: true }
         ]
       }
     }
