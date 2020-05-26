42b4e57620646790b844ad587859610f8a6641ee
Dan Weather <spamdrap@gmail.com>
2020-05-26 21:09:26 +0800

    Install vue slick carousel & hardcoded the footer

--- components/templates/vjlp3/TheFooter.vue
+++ components/templates/vjlp3/TheFooter.vue
@@ -1,23 +1,74 @@
 <template>
-  <div>
-    <nuxt />
-  </div>
+  <footer id="footer" class="animated fadeIn">
+    <div class="container-fluid pp">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="payment-providers"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="icons text-center">
+          <div class="icon">
+            <a href="https://casino.verajohn.com/about/responsible-gaming" target="blank">
+              <div class="age"></div>
+            </a>
+          </div>
+          <div class="icon">
+            <a href="https://www.gamstop.co.uk" target="blank">
+              <div class="gamstop"></div>
+            </a>
+          </div>
+          <div class="icon">
+            <a href="https://www.begambleaware.org" target="blank">
+              <div class="bga"></div>
+            </a>
+          </div>
+          <div class="icon">
+            <a href="https://www.gamblingtherapy.org/en" target="blank">
+              <div class="gt"></div>
+            </a>
+          </div>
+          <div class="icon">
+            <a
+              href="https://secure.gamblingcommission.gov.uk/PublicRegister/Search/Detail/39408"
+              target="blank"
+            >
+              <div class="ukgc"></div>
+            </a>
+          </div>
+          <div class="icon">
+            <a
+              href="https://www.authorisation.mga.org.mt/verification.aspx?lang=en&amp;company=c0e82570-bf83-4ecc-bda3-89ecb92d5b0e"
+              target="blank"
+            >
+              <div class="mga"></div>
+            </a>
+          </div>
+          <div class="icon">
+            <a href="https://www.gamcare.org.uk" target="blank">
+              <div class="gamcare"></div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="footer-content col-12 text-center">
+          <p class="text-center">
+            <a
+              href="https://casino.verajohn.com/about/responsible-gaming"
+              target="blank"
+            >Gambling can be addictive - please play responsibly</a>
+          </p>
+          <p style="font-size:14px; text-align:center">
+            The operator of this website is Dumarca Gaming Ltd. with registered address at The Emporium, Level 4, St Louis Street, Msida MSD 1421, Malta.
+            <br />This website is regulated by the Malta Gaming Authority and operated under the following license: MGA.B2C/169/2009.
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
-
-<script>
-export default {
-  footer () {
-    return {
-      script: [
-        { defer: true, src: 'https://files.vjpromo.com/bannerflow/lp3/brand_vj/js/after.js' },
-        { type: 'text/javascript', src: 'https://files.vjpromo.com/bannerflow/lp-core/brand_vj/js/form.js' },
-        { type: 'text/javascript', src: 'https://files.vjpromo.com/bannerflow/lp-core/brand_vj/js/footer.js' },
-        { defer: true, src: 'https://files.vjpromo.com/bannerflow/lp-core/js/popper.min.js' },
-        { defer: true, src: 'https://files.vjpromo.com/bannerflow/lp-core/js/bootstrap.min.js' },
-        { defer: true, src: 'https://files.vjpromo.com/bannerflow/lp-core/js/slick.min.js' },
-        { defer: true, src: 'https://files.vjpromo.com/bannerflow/lp-core/js/jquery.flexverticalcenter.js' }
-      ]
-    }
-  }
-}
-</script>
--- components/templates/vjlp3/TheGameSlider.vue
+++ components/templates/vjlp3/TheGameSlider.vue
@@ -3,30 +3,40 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <div class="slider">
-                    <a href="#" class="hvr-shrink">
-                        <div class="bf-game-icon-1"></div>
-                    </a>
+                    <VueSlickCarousel :arrows="false" :dots="false" :infinite="true" :speed="300" :slidesToShow="5" :slidesToScroll="1" :focusOnSelect="true">
                         <a href="#" class="hvr-shrink">
-                        <div class="bf-game-icon-2"></div>
-                    </a>
-                        <a href="#" class="hvr-shrink">
-                        <div class="bf-game-icon-3"></div>
-                    </a>
-                        <a href="#" class="hvr-shrink">
-                        <div class="bf-game-icon-4"></div>
-                    </a>
-                        <a href="#" class="hvr-shrink">
-                        <div class="bf-game-icon-5"></div>
-                    </a>
-                    </div>
+                            <div class="bf-game-icon-1"></div>
+                        </a>
+                            <a href="#" class="hvr-shrink">
+                            <div class="bf-game-icon-2"></div>
+                        </a>
+                            <a href="#" class="hvr-shrink">
+                            <div class="bf-game-icon-3"></div>
+                        </a>
+                            <a href="#" class="hvr-shrink">
+                            <div class="bf-game-icon-4"></div>
+                        </a>
+                            <a href="#" class="hvr-shrink">
+                            <div class="bf-game-icon-5"></div>
+                        </a>
+                    </VueSlickCarousel>
                 </div>
             </div>		 
         </div>
 	</section>
 </template>
-<style scoped>
-#bf-game-slider a div {
-    width: 218px;
-}
-</style>
+
+<script>
+  import VueSlickCarousel from 'vue-slick-carousel'
+ 
+  export default {
+    components: { VueSlickCarousel },
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 3,
+            infinite: true
+        }
+    }]
+  }
+</script>
