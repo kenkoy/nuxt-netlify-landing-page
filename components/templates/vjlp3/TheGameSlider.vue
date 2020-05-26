61dfe060bad5a5fc6bcab02e1a7dcddd396e6cc7
Dan Weather <spamdrap@gmail.com>
2020-05-26 22:44:39 +0800

    Vue slick carousel responsive settings

--- components/templates/vjlp3/TheGameSlider.vue
+++ components/templates/vjlp3/TheGameSlider.vue
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <VueSlickCarousel :arrows="false" :dots="false" :infinite="true" :speed="300" :slidesToShow="5" :slidesToScroll="1" :focusOnSelect="true">
+                    <VueSlickCarousel :arrows="false" :dots="false" :infinite="true" :speed="300" :slidesToShow="5" :slidesToScroll="1" :focusOnSelect="true" v-bind="carouselSettings">
                         <a href="#" class="hvr-shrink">
                             <div class="bf-game-icon-1"></div>
                         </a>
@@ -29,14 +29,23 @@
 <script>
   import VueSlickCarousel from 'vue-slick-carousel'
  
-  export default {
-    components: { VueSlickCarousel },
-    responsive: [{
-        breakpoint: 1024,
-        settings: {
-            slidesToShow: 3,
-            infinite: true
-        }
-    }]
+export default {
+  components: {
+      VueSlickCarousel
+  },
+  data(){
+    return{
+      carouselSettings:{
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                infinite: true,
+                autoplay: true
+            }
+        }]
+      }
+    }
   }
+}
 </script>
