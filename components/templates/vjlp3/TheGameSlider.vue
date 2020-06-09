4858188c91721f242a50d5853fd876733a79e820
Dan Weather <spamdrap@gmail.com>
2020-06-09 14:59:30 +0800

    Gameslider url converted into object

--- components/templates/vjlp3/TheGameSlider.vue
+++ components/templates/vjlp3/TheGameSlider.vue
@@ -13,123 +13,74 @@
             :focusOnSelect="true"
             v-bind="carouselSettings"
           >
-            <a class="hvr-shrink" v-bind:href="game_url_1">
+            <a class="hvr-shrink" v-bind:href="game.game_url_1">
               <div class="bf-game-icon-1"></div>
             </a>
-            <a class="hvr-shrink" v-bind:href="game_url_2">
+            <a class="hvr-shrink" v-bind:href="game.game_url_2">
               <div class="bf-game-icon-2"></div>
             </a>
-            <a class="hvr-shrink" v-bind:href="game_url_3">
+            <a class="hvr-shrink" v-bind:href="game.game_url_3">
               <div class="bf-game-icon-3"></div>
             </a>
-            <a class="hvr-shrink" v-bind:href="game_url_4">
+            <a class="hvr-shrink" v-bind:href="game.game_url_4">
               <div class="bf-game-icon-4"></div>
             </a>
-            <a class="hvr-shrink" v-bind:href="game_url_5">
+            <a class="hvr-shrink" v-bind:href="game.game_url_5">
               <div class="bf-game-icon-5"></div>
             </a>
           </VueSlickCarousel>
+          <style>
+            :root {
+                --bg-game-icon1: url('{{ game.game_icon_1 }}');
+                --bg-game-icon2: url('{{ game.game_icon_2 }}');
+                --bg-game-icon3: url('{{ game.game_icon_3 }}');
+                --bg-game-icon4: url('{{ game.game_icon_4 }}');
+                --bg-game-icon5: url('{{ game.game_icon_5 }}');
+                --bg-game-icon6: url('{{ game.game_icon_6 }}');
+            }
+        </style>
         </div>
       </div>
     </div>
   </section>
 </template>
 
+
+
 <script>
 import VueSlickCarousel from "vue-slick-carousel";
 
 export default {
-name: 'GameSlider',
-props: {
-    game_slider_icon_1: {
-        type: String,
-        required: true
+    name: 'GameSlider',
+    props: {
+        game: {
+            type: Object,
+            required: true
+        }
     },
-    game_url_1: {
-        type: String,
-        required: true
+
+    components: {
+        VueSlickCarousel,  
     },
-    game_slider_icon_2: {
-        type: String,
-        required: true
-    },
-    game_url_2: {
-        type: String,
-        required: true
-    },
-    game_slider_icon_3: {
-        type: String,
-        required: true
-    },
-    game_url_3: {
-        type: String,
-        required: true
-    },
-    game_slider_icon_4: {
-        type: String,
-        required: true
-    },
-    game_url_4: {
-        type: String,
-        required: true
-    },
-    game_slider_icon_5: {
-        type: String,
-        required: true
-    },
-    game_url_5: {
-        type: String,
-        required: true
-    },
-    game_slider_icon_6: {
-        type: String,
-        required: true
-    },
-    game_url_6: {
-        type: String,
-        required: true
-    }
-},
-components: {
-VueSlickCarousel,
-},
-async asyncData({ params }) {
-try {
-    const post = await import(
-    "~/assets/content/landing-page/vjlp3/" + params.slug + ".md"
-    );
-    console.log(post.attributes.game_images.game_url_1);
-    return {
-    post,
-    };
-} catch (error) {
-    return false;
-}
-},
-  data() {
-    return {
-      carouselSettings: {
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              infinite: true,
-              autoplay: true,
+    data() {
+        return {
+            carouselSettings: {
+                responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                    slidesToShow: 3,
+                    infinite: true,
+                    autoplay: true,
+                    },
+                },
+                ],
             },
-          },
-        ],
-      },
-    };
-  },
+        };
+    },
 };
 </script>
 <style>
-.bf-game-icon-1 { background: url("~assets/images/vjlp3/icon_1.png") center no-repeat;  background-size: contain; }
-.bf-game-icon-2 { background: url("~assets/images/vjlp3/icon_2.png") center no-repeat;  background-size: contain; }
-.bf-game-icon-3 { background: url("~assets/images/vjlp3/icon_3.png") center no-repeat;  background-size: contain; }
-.bf-game-icon-4 { background: url("~assets/images/vjlp3/icon_4.png") center no-repeat;  background-size: contain; }
-.bf-game-icon-5 { background: url("~assets/images/vjlp3/icon_5.png") center no-repeat;  background-size: contain; }
 .bf-game-icon-1 {
   background: var(--bg-game-icon1) 50% no-repeat;
   background-size: contain;
--- pages/vjlp3/_slug/index.vue
+++ pages/vjlp3/_slug/index.vue
@@ -10,32 +10,13 @@
       :promo_description = "post.attributes.promo_banner.promo_subtitle"
       :promo_join_button = "post.attributes.promo_banner.promo_join_button"
       :promo_join_button_redirect_url = "post.attributes.promo_banner.promo_join_button_redirect_url" />
-    <TheGameSlider 
-      :game_icon_1 = "post.attributes.promo_banner.game_images.game_icon_1"
-      :game_url_1 = "post.attributes.promo_banner.game_images.game_url_1"
-      :game_icon_2 = "post.attributes.promo_banner.game_images.game_icon_2"
-      :game_url_2 = "post.attributes.promo_banner.game_images.game_url_2"
-      :game_icon_3 = "post.attributes.promo_banner.game_images.game_icon_3"
-      :game_url_3 = "post.attributes.promo_banner.game_images.game_url_3"
-      :game_icon_4 = "post.attributes.promo_banner.game_images.game_icon_4"
-      :game_url_4 = "post.attributes.promo_banner.game_images.game_url_4"
-      :game_icon_5 = "post.attributes.promo_banner.game_images.game_icon_5"
-      :game_url_5 = "post.attributes.promo_banner.game_images.game_url_5"
-      :game_icon_6 = "post.attributes.promo_banner.game_images.game_icon_6"
-      :game_url_6 = "post.attributes.promo_banner.game_images.game_url_6"
-    />
+    <TheGameSlider :game = "post.attributes.promo_banner.game_images" />
     <TheBody :promo_content = "post.html"/>
     <TheFooter />
     <style>
       :root {
         --bg-image: url('{{ post.attributes.promo_banner.promo_images.promo_bg_desktop }}');
         --bg-image-m: url('{{ post.attributes.promo_banner.promo_images.promo_bg_mobile }}');
-        --bg-game-icon1: url('{{ post.attributes.promo_banner.game_images.game_icon_1 }}');
-        --bg-game-icon2: url('{{ post.attributes.promo_banner.game_images.game_icon_2 }}');
-        --bg-game-icon3: url('{{ post.attributes.promo_banner.game_images.game_icon_3 }}');
-        --bg-game-icon4: url('{{ post.attributes.promo_banner.game_images.game_icon_4 }}');
-        --bg-game-icon5: url('{{ post.attributes.promo_banner.game_images.game_icon_5 }}');
-        --bg-game-icon6: url('{{ post.attributes.promo_banner.game_images.game_icon_6 }}');
       }
     </style>
   </div>
@@ -60,7 +41,6 @@ export default {
     async asyncData ({ params }) {
       try {
         const post = await import('~/assets/content/landing-page/vjlp3/'+ params.slug +'.md')
-        console.log(post.attributes.promo_banner.promo_login_button_redirect_url)
         return {
           post
         }
@@ -73,11 +53,7 @@ export default {
 
 <style> 
   #bf-banner { background: linear-gradient(rgba(0, 0, 0, 0.15), rgba(0, 0, 0, 0.15)), url("~assets/images/vjlp3/bg.jpg") center no-repeat; background-size: cover; }
-  /* .bf-game-icon-1 { background: url("~assets/images/vjlp3/icon_1.png") center no-repeat;  background-size: contain; }
-  .bf-game-icon-2 { background: url("~assets/images/vjlp3/icon_2.png") center no-repeat;  background-size: contain; }
-  .bf-game-icon-3 { background: url("~assets/images/vjlp3/icon_3.png") center no-repeat;  background-size: contain; }
-  .bf-game-icon-4 { background: url("~assets/images/vjlp3/icon_4.png") center no-repeat;  background-size: contain; }
-  .bf-game-icon-5 { background: url("~assets/images/vjlp3/icon_5.png") center no-repeat;  background-size: contain; } */
+
   #strip .small-terms { display: block; } #strip { background:none; }
   #bf-banner-img-2 { 
     background: var(--bg-image) bottom center no-repeat; 
