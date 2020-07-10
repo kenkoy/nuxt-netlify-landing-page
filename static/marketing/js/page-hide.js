42cbf71dcd3b329516020f5108bff55765a81380
Mat Francisco <mat.francisco@gamesysgroup.com>
2020-07-10 13:50:03 +0800

    Removed the export default

--- static/marketing/js/page-hide.js
+++ static/marketing/js/page-hide.js
@@ -1,5 +1,4 @@
-
-export default (function (a, s, y, n, c, h, i, d, e) {
+(function (a, s, y, n, c, h, i, d, e) {
   s.className += ' ' + y; h.start = 1 * new Date;
   h.end = i = function () { s.className = s.className.replace(RegExp(' ?' + y), '') };
   (a[n] = a[n] || []).hide = h; setTimeout(function () { i(); h.end = null }, c); h.timeout = c;
