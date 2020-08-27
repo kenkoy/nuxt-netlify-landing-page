c9ff664729eb169194ffb5dad1081e10be21016b
Dan Weather <spamdrap@gmail.com>
2020-08-27 19:54:50 +0800

    Add header section fields

--- components/templates/vjhp/TheHeaderSection.vue
+++ components/templates/vjhp/TheHeaderSection.vue
@@ -1,76 +1,80 @@
 <template>
-  <div id="wrapper">
-    <header id="header">
-      <span class="logo">
-        <img src="/marketing/img/vjhp/logo.svg" alt />
-      </span>
-      <h2>ベラジョン・オンラインカジノへようこそ!</h2>
-      <h1>日本NO.1カジノ!</h1>
-      <h3>
-        圧倒的な人気とシェア!
-        <br />さらに安心・安全・楽しいの3拍子がばっちり揃う、業界ナンバーワンカジノで遊んじゃおう!
-      </h3>
-      <ul class="items-list gameslider">
-        <li>
-          <a href="#">
-            <img src="/marketing/img/vjhp/game1.svg" />
+  <section>
+    <div class="logInHeader">
+      {{ header_section_top_text }}
+      <a v-bind:href="header_section_top_login_text_url">
+        <span>{{ header_section_top_login_text }}</span>
+      </a>
+    </div>
+    <div id="wrapper" v-bind:style='{ backgroundImage: "url(" + header_section_background_image + ")", }'>
+      <header id="header">
+        <span class="logo">
+          <img src="/marketing/img/vjhp/logo.svg" alt />
+        </span>
+        <h2>{{ header_section_welcome_message }}</h2>
+        <h1>{{ header_section_main_heading }}</h1>
+        <h3>{{ header_section_heading_subtitle }}</h3>
+        <ul class="items-list gameslider">
+          <li>
+            <a v-bind:href="header_section_game_slider_url_1">
+              <img v-bind:src="header_section_game_slider_1" />
+            </a>
+          </li>
+          <li>
+            <a v-bind:href="header_section_game_slider_url_2">
+              <img v-bind:src="header_section_game_slider_2" />
+            </a>
+          </li>
+          <li>
+            <a v-bind:href="header_section_game_slider_url_3">
+              <img v-bind:src="header_section_game_slider_3" />
+            </a>
+          </li>
+          <li>
+            <a v-bind:href="header_section_game_slider_url_4">
+              <img v-bind:src="header_section_game_slider_4" />
+            </a>
+          </li>
+          <li>
+            <a v-bind:href="header_section_game_slider_url_5">
+              <img v-bind:src="header_section_game_slider_5" />
+            </a>
+          </li>
+          <li>
+            <a v-bind:href="header_section_game_slider_url_6">
+              <img v-bind:src="header_section_game_slider_6" />
+            </a>
+          </li>
+        </ul>
+        <h4>{{ header_section_best_deal_caption }}</h4>
+        <div class="chest">
+          <a v-bind:href="header_section_best_deal_image_url">
+            <img v-bind:src="header_section_best_deal_image" />
           </a>
-        </li>
-        <li>
-          <a href="#">
-            <img src="/marketing/img/vjhp/game2.svg" />
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <img src="/marketing/img/vjhp/game3.svg" />
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <img src="/marketing/img/vjhp/game4.svg" />
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <img src="/marketing/img/vjhp/game5.svg" />
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <img src="/marketing/img/vjhp/game6.svg" />
-          </a>
-        </li>
-      </ul>
-      <h4>登録してからすぐ使える! 業界一お得なビギナーズボーナスのおまけつき!</h4>
-      <div class="chest">
-        <a href="#">
-          <img src="/marketing/img/vjhp/chest.svg" />
-        </a>
-      </div>
-      <p>お試し登録ボーナス &amp; 最初3回のご入金に合計最大$1000ボーナスをプレゼント!</p>
-      <a class="button primary" v-bind:href="header_button_redirect_url">{{ header_text }}</a>
-      <p>
-        アカウントをお持ちの方はコチラから
-        <a v-bind:href="header_login_account_text">{{ header_login_account_text_redirect_url }}</a>
-      </p>
-    </header>
-  </div>
+        </div>
+        <p>{{ header_section_best_deal_small_caption }}</p>
+        <a class="button primary" v-bind:href="header_section_register_button_url">{{ header_section_register_button }}</a>
+        <p>
+          {{ header_section_account_text }}
+          <a v-bind:href="header_section_account_login_url">{{ header_section_account_login }}</a>
+        </p>
+      </header>
+    </div>
+  </section>
 </template>
-
 <script>
 export default {
   name: 'TheHeaderSection',
   props: {
-    header_text: {
+    header_section_top_text: {
       type: String,
       required: true,
     },
-    header_button_redirect_url: {
+    header_section_top_login_text: {
       type: String,
       required: true,
     },
-    header_login_account_text: {
+    header_section_top_login_text_url: {
       type: String,
       required: true,
     },
@@ -82,9 +86,106 @@ export default {
       type: String,
       required: true,
     },
+    header_section_welcome_message: {
+      type: String,
+      required: true,
+    },
+    header_section_main_heading: {
+      type: String,
+      required: true,
+    },
+    header_section_heading_subtitle: {
+      type: String,
+      required: true,
+    },
+    header_section_game_slider_1: {
+       type: Image,
+       required: true,
+    },
+    header_section_game_slider_url_1: {
+      type: String,
+      required: true,
+    },
+    header_section_game_slider_2: {
+       type: Image,
+       required: true,
+    },
+    header_section_game_slider_url_2: {
+      type: String,
+      required: true,
+    },
+    header_section_game_slider_3: {
+       type: Image,
+       required: true,
+    },
+    header_section_game_slider_url_3: {
+      type: String,
+      required: true,
+    },
+    header_section_game_slider_4: {
+       type: Image,
+       required: true,
+    },
+    header_section_game_slider_url_4: {
+      type: String,
+      required: true,
+    },
+    header_section_game_slider_5: {
+       type: Image,
+       required: true,
+    },
+    header_section_game_slider_url_5: {
+      type: String,
+      required: true,
+    },
+    header_section_game_slider_6: {
+       type: Image,
+       required: true,
+    },
+    header_section_game_slider_url_6: {
+      type: String,
+      required: true,
+    },
+    header_section_best_deal_caption: {
+      type: String,
+      required: true,
+    },
+    header_section_best_deal_image_url: {
+      type: String,
+      required: true,
+    },
+    header_section_best_deal_image: {
+       type: Image,
+       required: true,
+    },
+    header_section_best_deal_small_caption: {
+      type: String,
+      required: true,
+    },
+    header_section_register_button: {
+      type: String,
+      required: true,
+    },
+    header_section_register_button_url: {
+      type: String,
+      required: true,
+    },
+    header_section_account_text: {
+      type: String,
+      required: true,
+    },
+    header_section_account_login: {
+      type: String,
+      required: true,
+    },
+    header_section_account_login_url: {
+      type: String,
+      required: true,
+    },
+    header_section_background_image: {
+      type: Image,
+      required: true,
+    }
   },
 };
 </script>
-
-<style>
-</style>
--- pages/marketing/vjhp/index.vue
+++ pages/marketing/vjhp/index.vue
@@ -1,16 +1,35 @@
 <template>
   <div>
-    <TheLoginHeaderSection
-      :title="post.attributes.promo_banner.promo_title"
-      :login_header_text="post.attributes.login_header_section.login_header_text"
-      :login_header_redirect_url="post.attributes.login_header_section.login_header_redirect_url"
-    />
     <TheHeaderSection
       :homepage_title="post.attributes.header_section.homepage_title || '' "
-      :header_text="post.attributes.header_section.header_text"
-      :header_button_redirect_url="post.attributes.header_section.header_button_redirect_url"
-      :header_login_account_text="post.attributes.header_section.header_login_account_text"
-      :header_login_account_text_redirect_url="post.attributes.header_section.header_login_account_text_redirect_url"
+      :header_section_top_text="post.attributes.header_section.header_section_top_text"
+      :header_section_top_login_text="post.attributes.header_section.header_section_top_login_text"
+      :header_section_top_login_text_url="post.attributes.header_section.header_section_top_login_text_url"
+      :header_section_welcome_message="post.attributes.header_section.header_section_welcome_message"
+      :header_section_main_heading="post.attributes.header_section.header_section_main_heading"
+      :header_section_heading_subtitle="post.attributes.header_section.header_section_heading_subtitle"
+      :header_section_game_slider_1="post.attributes.header_section.header_section_game_slider_1"
+      :header_section_game_slider_url_1="post.attributes.header_section.header_section_game_slider_url_1"
+      :header_section_game_slider_2="post.attributes.header_section.header_section_game_slider_2"
+      :header_section_game_slider_url_2="post.attributes.header_section.header_section_game_slider_url_2"
+      :header_section_game_slider_3="post.attributes.header_section.header_section_game_slider_3"
+      :header_section_game_slider_url_3="post.attributes.header_section.header_section_game_slider_url_3"
+      :header_section_game_slider_4="post.attributes.header_section.header_section_game_slider_4"
+      :header_section_game_slider_url_4="post.attributes.header_section.header_section_game_slider_url_4"
+      :header_section_game_slider_5="post.attributes.header_section.header_section_game_slider_5"
+      :header_section_game_slider_url_5="post.attributes.header_section.header_section_game_slider_url_5"
+      :header_section_game_slider_6="post.attributes.header_section.header_section_game_slider_6"
+      :header_section_game_slider_url_6="post.attributes.header_section.header_section_game_slider_url_6"
+      :header_section_best_deal_caption="post.attributes.header_section.header_section_best_deal_caption"
+      :header_section_best_deal_image="post.attributes.header_section.header_section_best_deal_image"
+      :header_section_best_deal_image_url="post.attributes.header_section.header_section_best_deal_image_url"
+      :header_section_best_deal_small_caption="post.attributes.header_section.header_section_best_deal_small_caption"
+      :header_section_register_button="post.attributes.header_section.header_section_register_button"
+      :header_section_register_button_url="post.attributes.header_section.header_section_register_button_url"
+      :header_section_account_text="post.attributes.header_section.header_section_account_text"
+      :header_section_account_login="post.attributes.header_section.header_section_account_login"
+      :header_section_account_login_url="post.attributes.header_section.header_section_account_login_url"
+      :header_section_background_image="post.attributes.header_section.header_section_background_image"
     />
     <TheSection2 />
     <TheGamesBrickSection
