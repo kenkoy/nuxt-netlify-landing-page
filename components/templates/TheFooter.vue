39bb88213d52bb54446f546420a7e795f1eb5d1c
Mat Francisco <mat.francisco@gamesysgroup.com>
2020-09-14 08:51:08 +0800

    Added HTML anchor labels to improve accessibility

--- components/templates/TheFooter.vue
+++ components/templates/TheFooter.vue
@@ -5,37 +5,42 @@
          <div class="row">
             <div class="icons text-center">
                <div class="icon">
-                  <a href="https://www.verajohn.com/about/responsible-gaming" target="blank">
+                  <a href="https://www.verajohn.com/about/responsible-gaming" target="blank"
+                     aria-label="Responsible Gaming">
                      <div class="age"></div>
                   </a>
                </div>
                <div class="icon">
-                  <a href="https://www.gamstop.co.uk" target="blank">
+                  <a href="https://www.gamstop.co.uk" target="blank"
+                     aria-label="GamStop">
                      <div class="gamstop"></div>
                   </a>
                </div>
                <div class="icon">
-                  <a href="https://www.begambleaware.org" target="blank">
+                  <a href="https://www.begambleaware.org" target="blank"
+                     aria-label="Be Gamble Aware">
                      <div class="bga"></div>
                   </a>
                </div>
                <div class="icon">
-                  <a href="https://www.gamblingtherapy.org/en" target="blank">
+                  <a href="https://www.gamblingtherapy.org/en" target="blank"
+                     aria-label="Gambling Theraphy">
                      <div class="gt"></div>
                   </a>
                </div>
                <div class="icon">
-                  <a href="https://secure.gamblingcommission.gov.uk/PublicRegister/Search/Detail/39408" target="blank">
+                  <a href="https://secure.gamblingcommission.gov.uk/PublicRegister/Search/Detail/39408" target="blank"
+                     aria-label="Gambling Commission">
                      <div class="ukgc"></div>
                   </a>
                </div>
                <div class="icon">
-                  <a href="#" target="blank">
+                  <a href="#" target="blank" aria-label="Curacao">
                      <div class="curacao"></div>
                   </a>
                </div>
                <div class="icon">
-                  <a href="https://www.gamcare.org.uk" target="blank">
+                  <a href="https://www.gamcare.org.uk" target="blank" aria-label="GamCare">
                      <div class="gamcare"></div>
                   </a>
                </div>
@@ -44,7 +49,8 @@
          <div class="row">
             <div class="footer-content col-12 text-center">
             <p class="text-center">
-               <a href="https://verajohn.com/about/responsible-gaming" target="blank">ギャンブルには中毒性があります。 自己責任を持ってプレイしてください。</a>
+               <a href="https://verajohn.com/about/responsible-gaming" target="blank"
+                  aria-label="Responsible Gaming">ギャンブルには中毒性があります。 自己責任を持ってプレイしてください。</a>
             </p>
             <p style="font-size:14px; text-align:center">
                当ウェブサイトは、キュラソー政府の商業登記に登録されている、番号149132のBreckenridge Curacao B.V.（登録住所：Emancipatie Boulevard, Dominico F. “Don” Martina 31, Willemstad, Curaçao）により運営されています。<br>Breckenridge Curaçao B.V.はキュラソー州知事により発行されたMaster Gaming License #5536/JAZに基づく C.I.L Curacao Interavtive Licensing N.V. により付与されたサブライセンスを通して正式な許可を所持しています
@@ -68,37 +74,43 @@
       <div class="row">
          <div class="icons text-center">
             <div class="icon">
-               <a href="https://verajohn.com/about/responsible-gaming" target="blank">
+               <a href="https://verajohn.com/about/responsible-gaming" target="blank"
+                  aria-label="Responsible Gaming">
                   <div class="age"></div>
                </a>
             </div>
             <div class="icon">
-               <a href="https://www.gamstop.co.uk" target="blank">
+               <a href="https://www.gamstop.co.uk" target="blank"
+                  aria-label="GamStop">
                   <div class="gamstop"></div>
                </a>
             </div>
             <div class="icon">
-               <a href="https://www.begambleaware.org" target="blank">
+               <a href="https://www.begambleaware.org" target="blank"
+                  aria-label="Be Gamble Aware">
                   <div class="bga"></div>
                </a>
             </div>
             <div class="icon">
-               <a href="https://www.gamblingtherapy.org/en" target="blank">
+               <a href="https://www.gamblingtherapy.org/en" target="blank"
+                  aria-label="Gambling Theraphy">
                   <div class="gt"></div>
                </a>
             </div>
             <div class="icon">
-               <a href="https://secure.gamblingcommission.gov.uk/PublicRegister/Search/Detail/39408" target="blank">
+               <a href="https://secure.gamblingcommission.gov.uk/PublicRegister/Search/Detail/39408" target="blank"
+                  aria-label="Gambling Commission">
                   <div class="ukgc"></div>
                </a>
             </div>
             <div class="icon">
-               <a href="https://www.authorisation.mga.org.mt/verification.aspx?lang=en&company=c0e82570-bf83-4ecc-bda3-89ecb92d5b0e" target="blank">
+               <a href="https://www.authorisation.mga.org.mt/verification.aspx?lang=en&company=c0e82570-bf83-4ecc-bda3-89ecb92d5b0e" target="blank"
+                  aria-label="Malta Gaming Authority">
                   <div class="mga"></div>
                </a>
             </div>
             <div class="icon">
-               <a href="https://www.gamcare.org.uk" target="blank">
+               <a href="https://www.gamcare.org.uk" target="blank" aria-label="GamCare">
                   <div class="gamcare"></div>
                </a>
             </div>
