000320610fba7a95b36a00f6776eca219bd82d2d
Mat Francisco <mat.francisco@gamesysgroup.com>
2020-09-29 09:16:08 +0800

    Removed unnecessary comments

--- components/templates/iclp/iclp3/TheBonus.vue
+++ components/templates/iclp/iclp3/TheBonus.vue
@@ -1,64 +1,6 @@
 <template>
   <div class="bonus">
     <section class="campaign">
-      <!-- <div class="campaign__box">
-        <h3 class="campaign__box__title">
-          <img
-            src="/marketing/img/iclp3/welcome_bonus.png"
-            alt="welcome-bonus"
-            v-lazy-load
-          />
-        </h3>
-        <p class="campaign__box__summary">
-          新規プレイヤー限定!
-          <br />入金してキャッシュでのプレイに応じて
-          <br />
-          <b>総額$500</b>のキャッシュが貰えるチャンス!
-        </p>
-        <a class="campaign__box__item__link" href="https://www.intercasino.com/#join" alt="今すぐ登録">
-          <img
-            src="/marketing/img/iclp3/signup_button.png"
-            alt="signup"
-            v-lazy-load
-          />
-        </a>
-      </div> -->
-
-      <!-- <div class="campaign__box twitter">
-        <h3 class="campaign__box__title">
-          <img
-            src="/marketing/img/iclp3/present_CP.png"
-            alt="present"
-            v-lazy-load
-          />
-        </h3>
-        <p class="campaign__box__summary">
-          姉妹カジノの
-          <br class="sp" />ベラジョンカジノの
-          <a href="https://twitter.com/Japan_VeraJohn" target="_blank" rel="noopener">公式Twitter</a>で、
-          <br />
-          <b>
-            限定オリジナルグッズの
-            <br class="sp" />抽選会を実施中！
-          </b>
-          <br />詳細はベラジョンカジノの
-          <br />
-          <a href="https://twitter.com/Japan_VeraJohn" target="_blank" rel="noopener">公式Twitterアカウント</a>をチェック！
-        </p>
-        <a
-          class="campaign__box__item__link"
-          href="https://twitter.com/Japan_VeraJohn"
-          target="_blank"
-          rel="noopener"
-        >
-          <img
-            src="/marketing/img/iclp3/twitter_button.png"
-            alt="今すぐtweet"
-            v-lazy-load
-          />
-        </a>
-      </div> -->
-
       <div v-for="(datum, index) of bonusData" :key="index">
         <div class="campaign__box twitter">
           <h3 class="campaign__box__title">
@@ -120,7 +62,8 @@ export default {
           .forEach(phrase => {
             while (summary.length > idx_2) {
               if (summary[idx_2].includes(phrase.text)) {
-                summary[idx_2] = summary[idx_2].replace(phrase.text, `<a href="${phrase.link}" target="_blank" rel="noopener">${phrase.text}</a>`);
+                summary[idx_2] = summary[idx_2].replace(phrase.text,
+                  `<a href="${phrase.link}" target="_blank" rel="noopener">${phrase.text}</a>`);
                 break;
               } else {
                 idx_2++;
