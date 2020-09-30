041944bbe8dfeb1ccad60b6d4fb54dae70863668
Dan Weather <spamdrap@gmail.com>
2020-09-30 15:16:48 +0800

    Edit section2 image

--- components/templates/vjhp/TheSection2.vue
+++ components/templates/vjhp/TheSection2.vue
@@ -3,7 +3,7 @@
     <header class="heading">
       <h2>プロ厳選! 決済方法</h2>
       <div>
-        <img src="/marketing/img/vjhp/footerPayment.png" alt=""/>
+        <img src="/marketing/img/vjhp/footerPayment.png" class="support-payment" alt="Payment"/>
       </div>
       <p>
         ベラジョンに導入されている決済方法は、その道のプロが厳選した便利で安心の決済方法のみ!
