d879bbe557a9a594c65c50c85f90dbb8cf3cca14
Niño Estole <antonino.estole@jpj.com>
2020-10-02 21:35:10 +0800

    Revert "merge latest changes from GitLab Master"
    This reverts commit 20e5c6dc45b5cde59291f37522e12b7a835d5528.

---
+++ .history/components/templates/ThePostList_20200719215206.vue
@@ -0,0 +1,44 @@
+<template>    
+    <section class="featured-posts">
+        <ThePostPreview v-for="post in posts"
+            :key="post.slug"
+            :slug="post.slug"
+            :path="post.path"
+            :title="post.attributes.promo_banner.promo_title"
+            :description="post.attributes.promo_banner.promo_subtitle"
+        />
+    </section>    
+</template>
+
+<script>
+import ThePostPreview from '~/components/templates/ThePostPreview'
+
+export default {
+    components: {
+        ThePostPreview
+    },
+    props: {
+        posts: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.post-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.featured-posts {
+    display: flex;
+    padding: 20px;
+    box-sizing: border-box;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+}
+</style>
