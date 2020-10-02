2f452c1cb609da933ee3869a9e99e0b4a483c1ed
Niño Estole <antonino.estole@jpj.com>
2020-10-02 22:46:14 +0800

    INVESTIGATION: Performance issues

--- components/templates/vjhp/TheSection7.vue
+++ components/templates/vjhp/TheSection7.vue
@@ -4,12 +4,12 @@
       <li>
         <h3>決済方法</h3>
         <p>お好きな方法で入出金がラクラク簡単!</p>
-        
+        <img src="/marketing/img/vjhp/footerPayment.png" class="footer-payment" alt="Payment"/>
       </li>
       <li>
         <h3>なにかでお困りですか?</h3>
         <p>日本語カスタマーサポートがお手伝いいたします。</p>
-        
+        <img src="/marketing/img/vjhp/footerSupport.png" class="footer-support" alt="Support"/>
       </li>
     </ul>
   </section>
