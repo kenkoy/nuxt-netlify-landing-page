a98e9dee9a20b17aa8c37df9d0cd05684c78aec1
Niño Estole <antonino.estole@jpj.com>
2020-10-02 22:26:55 +0800

    INVESTIGATION: Performance issues

--- components/templates/vjhp/TheSection5.vue
+++ components/templates/vjhp/TheSection5.vue
@@ -8,12 +8,12 @@
           <br />各種ゲームの探し方から遊び方、入出金方法などを
           <br />完全網羅する「ビギナーズガイド」がお手伝いします!
         </p>
-        <img src="/marketing/img/vjhp/reward-component_1.png" alt="" width="344"/>
+        
       </li>
       <li class="style1">
         <h3>ご褒美プログラム</h3>
         <p>楽しく遊んでコインを集めて、ベラジョン・ショッ プでお買い物! フリースピンやスピンクレジット、キャッシュアイ テムとも交換できちゃいます!</p>
-        <img src="/marketing/img/vjhp/reward-component_2.png" alt="" width="339"/>
+        
       </li>
       <li class="style1">
         <h3>毎日開催トーナメント</h3>
@@ -21,7 +21,7 @@
           勝ち方いろいろ、賞金いろいろ、多種多様なトーナ
           <br />メントが毎日開催! 得意なゲームでいざ勝負!
         </p>
-         <img src="/marketing/img/vjhp/reward-component_3.png" alt="" width="304"/>
+        
       </li>
     </ul>
   </section>
