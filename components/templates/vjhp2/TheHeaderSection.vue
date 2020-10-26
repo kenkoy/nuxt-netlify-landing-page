3b2d0861cf9d64992759432514fd68f487cb5825
Niño Estole <antonino.estole@jpj.com>
2020-10-26 19:35:06 +0800

    VJHP 2.0 page added

---
+++ components/templates/vjhp2/TheHeaderSection.vue
@@ -0,0 +1,217 @@
+<template>
+  <section>
+    <div class="logInHeader" v-bind:style='{ backgroundColor: header_section_top_background_color }'>
+      {{ header_section_top_text }}
+      <a v-bind:href="header_section_top_login_text_url">
+        <span>{{ header_section_top_login_text }}</span>
+      </a>
+    </div>
+    <div id="wrapper">
+      <header id="header">
+        <span class="logo">
+          <img src="/marketing/img/vjhp/logo.svg" alt="logo"/>
+        </span>
+        <h2>{{ header_section_welcome_message }}</h2>
+        <h1>{{ header_section_main_heading }}</h1>
+        <h3>{{ header_section_heading_subtitle }}</h3>
+        <ul class="items-list gameslider">
+          <li>
+            <a v-bind:href="header_section_game_slider_url_1">
+              <img v-bind:src="header_section_game_slider_1" alt="game1" v-lazy-load/>
+            </a>
+          </li>
+          <li>
+            <a v-bind:href="header_section_game_slider_url_2">
+              <img v-bind:src="header_section_game_slider_2" alt="game2" v-lazy-load/>
+            </a>
+          </li>
+          <li>
+            <a v-bind:href="header_section_game_slider_url_3">
+              <img v-bind:src="header_section_game_slider_3" alt="game3" v-lazy-load/>
+            </a>
+          </li>
+          <li>
+            <a v-bind:href="header_section_game_slider_url_4">
+              <img v-bind:src="header_section_game_slider_4" alt="game4" v-lazy-load/>
+            </a>
+          </li>
+          <li>
+            <a v-bind:href="header_section_game_slider_url_5">
+              <img v-bind:src="header_section_game_slider_5" alt="game5" v-lazy-load/>
+            </a>
+          </li>
+          <li>
+            <a v-bind:href="header_section_game_slider_url_6">
+              <img v-bind:src="header_section_game_slider_6" alt="game6" v-lazy-load/>
+            </a>
+          </li>
+        </ul>
+        <h4>{{ header_section_best_deal_caption }}</h4>
+        <div class="chest">
+          <a v-bind:href="header_section_best_deal_image_url">
+            <img class="img-fluid" v-bind:src="header_section_best_deal_image" alt="image" v-lazy-load/>
+          </a>
+        </div>
+        <p>{{ header_section_best_deal_small_caption }}</p>
+        <a class="button primary" v-bind:href="header_section_register_button_url">{{ header_section_register_button }}</a>
+        <p>
+          {{ header_section_account_text }}
+          <a v-bind:href="header_section_account_login_url">{{ header_section_account_login }}</a>
+        </p>
+      </header>
+    </div>
+    <style>
+      :root {
+        --bg-desktop: url('{{ header_section_desktop_background_image }}');
+        --bg-mobile: url('{{ header_section_mobile_background_image }}');
+      }
+    </style>
+  </section>
+</template>
+<script>
+export default {
+  name: 'TheHeaderSection',
+  props: {
+    header_section_top_text: {
+      type: String,
+      required: true,
+    },
+    header_section_top_login_text: {
+      type: String,
+      required: true,
+    },
+    header_section_top_login_text_url: {
+      type: String,
+      required: true,
+    },
+    header_section_top_background_color: {
+      type: String,
+      required: true,
+    },
+    header_login_account_text_redirect_url: {
+      type: String,
+      required: true,
+    },
+    homepage_title: {
+      type: String,
+      required: true,
+    },
+    header_section_welcome_message: {
+      type: String,
+      required: true,
+    },
+    header_section_main_heading: {
+      type: String,
+      required: true,
+    },
+    header_section_heading_subtitle: {
+      type: String,
+      required: true,
+    },
+    header_section_game_slider_1: {
+       type: String,
+       required: true,
+    },
+    header_section_game_slider_url_1: {
+      type: String,
+      required: true,
+    },
+    header_section_game_slider_2: {
+       type: String,
+       required: true,
+    },
+    header_section_game_slider_url_2: {
+      type: String,
+      required: true,
+    },
+    header_section_game_slider_3: {
+       type: String,
+       required: true,
+    },
+    header_section_game_slider_url_3: {
+      type: String,
+      required: true,
+    },
+    header_section_game_slider_4: {
+       type: String,
+       required: true,
+    },
+    header_section_game_slider_url_4: {
+      type: String,
+      required: true,
+    },
+    header_section_game_slider_5: {
+       type: String,
+       required: true,
+    },
+    header_section_game_slider_url_5: {
+      type: String,
+      required: true,
+    },
+    header_section_game_slider_6: {
+       type: String,
+       required: true,
+    },
+    header_section_game_slider_url_6: {
+      type: String,
+      required: true,
+    },
+    header_section_best_deal_caption: {
+      type: String,
+      required: true,
+    },
+    header_section_best_deal_image_url: {
+      type: String,
+      required: true,
+    },
+    header_section_best_deal_image: {
+       type: String,
+       required: true,
+    },
+    header_section_best_deal_small_caption: {
+      type: String,
+      required: true,
+    },
+    header_section_register_button: {
+      type: String,
+      required: true,
+    },
+    header_section_register_button_url: {
+      type: String,
+      required: true,
+    },
+    header_section_account_text: {
+      type: String,
+      required: true,
+    },
+    header_section_account_login: {
+      type: String,
+      required: true,
+    },
+    header_section_account_login_url: {
+      type: String,
+      required: true,
+    },
+    header_section_desktop_background_image: {
+      type: String,
+      required: true,
+    },
+    header_section_mobile_background_image: {
+      type: String,
+      required: true,
+    }
+  },
+};
+</script>
+<style>
+#wrapper {
+  background-image: var(--bg-desktop);
+}
+
+@media screen and (max-width: 767px) {
+  #wrapper {
+    background-image: var(--bg-mobile);
+    background-position: center center;
+  }
+}
+</style>
---
+++ pages/marketing/vjhp2/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <TheHeaderSection
+      :homepage_title="post.attributes.header_section.homepage_title || '' "
+      :header_section_top_text="post.attributes.header_section.header_section_top_text"
+      :header_section_top_login_text="post.attributes.header_section.header_section_top_login_text"
+      :header_section_top_login_text_url="post.attributes.header_section.header_section_top_login_text_url"
+      :header_section_welcome_message="post.attributes.header_section.header_section_welcome_message"
+      :header_section_main_heading="post.attributes.header_section.header_section_main_heading"
+      :header_section_heading_subtitle="post.attributes.header_section.header_section_heading_subtitle"
+      :header_section_game_slider_1="post.attributes.header_section.header_section_game_slider_1"
+      :header_section_game_slider_url_1="post.attributes.header_section.header_section_game_slider_url_1"
+      :header_section_game_slider_2="post.attributes.header_section.header_section_game_slider_2"
+      :header_section_game_slider_url_2="post.attributes.header_section.header_section_game_slider_url_2"
+      :header_section_game_slider_3="post.attributes.header_section.header_section_game_slider_3"
+      :header_section_game_slider_url_3="post.attributes.header_section.header_section_game_slider_url_3"
+      :header_section_game_slider_4="post.attributes.header_section.header_section_game_slider_4"
+      :header_section_game_slider_url_4="post.attributes.header_section.header_section_game_slider_url_4"
+      :header_section_game_slider_5="post.attributes.header_section.header_section_game_slider_5"
+      :header_section_game_slider_url_5="post.attributes.header_section.header_section_game_slider_url_5"
+      :header_section_game_slider_6="post.attributes.header_section.header_section_game_slider_6"
+      :header_section_game_slider_url_6="post.attributes.header_section.header_section_game_slider_url_6"
+      :header_section_best_deal_caption="post.attributes.header_section.header_section_best_deal_caption"
+      :header_section_best_deal_image="post.attributes.header_section.header_section_best_deal_image"
+      :header_section_best_deal_image_url="post.attributes.header_section.header_section_best_deal_image_url"
+      :header_section_best_deal_small_caption="post.attributes.header_section.header_section_best_deal_small_caption"
+      :header_section_register_button="post.attributes.header_section.header_section_register_button"
+      :header_section_register_button_url="post.attributes.header_section.header_section_register_button_url"
+      :header_section_account_text="post.attributes.header_section.header_section_account_text"
+      :header_section_account_login="post.attributes.header_section.header_section_account_login"
+      :header_section_account_login_url="post.attributes.header_section.header_section_account_login_url"
+      :header_section_desktop_background_image="post.attributes.header_section.header_section_desktop_background_image"
+      :header_section_mobile_background_image="post.attributes.header_section.header_section_mobile_background_image"
+      :header_section_top_background_color="post.attributes.header_section.header_section_top_background_color"
+      :header_login_account_text_redirect_url="post.attributes.header_section.header_login_account_text_redirect_url"
+    />
+  </div>
+</template>
+
+<script>
+import TheHeaderSection from '~/components/templates/vjhp2/TheHeaderSection.vue';
+
+export default {
+  layout: 'vjhp',
+  head() {
+    return { 
+      title: 'Vera&John - The fun online casino',
+      htmlAttrs: {
+        lang: 'ja',
+      },
+       bodyAttrs: {
+        id: 'ja-jp',
+      },
+      style: [],
+      script: [
+        { src: '/marketing/js/page-hide.js', id: 'go_data', go_id: 'OPT-PHSNXP6' },
+        { src: '/marketing/js/google-analytics.js', id: 'ga_data', go_id: 'OPT-PHSNXP6', ga_id: 'UA-142143961-1' },
+        { src: '/marketing/js/google-tag-manager.js', id: 'gtm_data', gtm_id: 'GTM-MFD3NKM'},
+      ],
+      noscript: [
+        {
+          hid: 'gtmBody',
+          innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id='GTM-MFD3NKM'" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          pbody: true
+        },
+      ],
+    }
+  },
+  components: {
+    TheHeaderSection
+  },
+  async asyncData() {
+    try {
+      const post = await import(
+        '~/assets/content/landing-page/marketing/vjhp/vera-john-homepage.md'
+      );
+      return {
+        post,
+      };
+    } catch (error) {
+      return false;
+    }
+  },
+};
+</script>
