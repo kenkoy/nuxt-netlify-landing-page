ba7d28b82fb733a4a24c3ed51afff5fd3e1ef789
Mat Francisco <mat.francisco@gamesysgroup.com>
2020-11-18 12:39:54 +0800

    Removed unwanted test files and fixed VJLP5 lint issues

--- layouts/vjlp5.vue
+++ layouts/vjlp5.vue
@@ -144,6 +144,16 @@ export default {
       mobile: ''
     }
   },
+  head () {
+    return {
+      htmlAttrs: {
+        lang: this.language
+      },
+      link: [
+        { rel: 'shortcut icon', href: '/marketing/vj-favicon.ico', type: 'image/x-icon' }
+      ]
+    }
+  },
   computed: {
     cssBackground () {
       return {
@@ -168,24 +178,14 @@ export default {
         this.mobile = item.attributes.promo_banner.promo_images.promo_bg_mobile
       })
     }
-  },
-  head () {
-    return {
-      htmlAttrs: {
-        lang: this.language
-      },
-      link: [
-        { rel: 'shortcut icon', href: '/marketing/vj-favicon.ico', type: 'image/x-icon' }
-      ]
-    }
   }
 }
 </script>
 
 <style lang="scss">
-    .vjlp5_wrapper {
-        @import '@/assets/sass/base/fonts.css';
-        @import '@/assets/sass/vjlp5/style.scss';
-        @import '@/assets/sass/base/style.scss';
-    }
+.vjlp5_wrapper {
+  @import '@/assets/sass/base/fonts.css';
+  @import '@/assets/sass/vjlp5/style.scss';
+  @import '@/assets/sass/base/style.scss';
+}
 </style>
