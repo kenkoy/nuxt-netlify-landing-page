21e86c7cfffef87f5781377554fdda1233d66c7d
Nathan Lerit <nathan.lerit@gamesysgroup.com>
2020-12-01 13:02:13 +0800

    update lighthouse score for vjhp2

--- layouts/vjhp2.vue
+++ layouts/vjhp2.vue
@@ -25,28 +25,28 @@
               </p>
               <div class="game-slider">
                 <a aria-label="link" :href="data_items.header_section.header_section_game_slider_url_1">
-                  <img :src="require(`~/assets/images${data_items.header_section.header_section_game_slider_1}`)" alt="alt img">
+                  <img width="100" height="100" :src="require(`~/assets/images${data_items.header_section.header_section_game_slider_1}`)" alt="alt img">
                 </a>
                 <a aria-label="link" :href="data_items.header_section.header_section_game_slider_url_2">
-                  <img :src="require(`~/assets/images${data_items.header_section.header_section_game_slider_2}`)" alt="alt img">
+                  <img width="100" height="100" :src="require(`~/assets/images${data_items.header_section.header_section_game_slider_2}`)" alt="alt img">
                 </a>
                 <a aria-label="link" :href="data_items.header_section.header_section_game_slider_url_3">
-                  <img :src="require(`~/assets/images${data_items.header_section.header_section_game_slider_3}`)" alt="alt img">
+                  <img width="100" height="100" :src="require(`~/assets/images${data_items.header_section.header_section_game_slider_3}`)" alt="alt img">
                 </a>
                 <a aria-label="link" :href="data_items.header_section.header_section_game_slider_url_4">
-                  <img :src="require(`~/assets/images${data_items.header_section.header_section_game_slider_4}`)" alt="alt img">
+                  <img width="100" height="100" :src="require(`~/assets/images${data_items.header_section.header_section_game_slider_4}`)" alt="alt img">
                 </a>
                 <a aria-label="link" :href="data_items.header_section.header_section_game_slider_url_5">
-                  <img :src="require(`~/assets/images${data_items.header_section.header_section_game_slider_5}`)" alt="alt img">
+                  <img width="100" height="100" :src="require(`~/assets/images${data_items.header_section.header_section_game_slider_5}`)" alt="alt img">
                 </a>
                 <a aria-label="link" :href="data_items.header_section.header_section_game_slider_url_6">
-                  <img :src="require(`~/assets/images${data_items.header_section.header_section_game_slider_6}`)" alt="alt img">
+                  <img width="100" height="100" :src="require(`~/assets/images${data_items.header_section.header_section_game_slider_6}`)" alt="alt img">
                 </a>
               </div>
               <p class="best-deal">
                 {{ data_items.header_section.header_section_best_deal_caption }}
               </p>
-              <img :src="require(`~/assets/images${data_items.header_section.header_section_best_deal_image}`)" alt="alt img">
+              <img width="200" height="200" :src="require(`~/assets/images${data_items.header_section.header_section_best_deal_image}`)" alt="alt img">
               <p class="small-caption">
                 {{ data_items.header_section.header_section_best_deal_small_caption }}
               </p>
@@ -63,7 +63,7 @@
           <div class="container">
             <div>
               <h2>プロ厳選! 決済方法</h2>
-              <img src="@/assets/images/vjhp2/footerPayment.png" alt="alt img">
+              <img width="324" height="167" src="@/assets/images/vjhp2/footerPayment.png" alt="alt img">
               <p>ベラジョンに導入されている決済方法は、その道のプロが厳選した便利で安心の決済方法のみ!</p>
             </div>
           </div>
@@ -72,7 +72,7 @@
           <div class="container column-2">
             <div>
               <h3>生配信で楽しめる! ライブカジノ</h3>
-              <img src="@/assets/images/vjhp2/mobile.png" alt="alt img">
+              <img width="458" height="458" src="@/assets/images/vjhp2/mobile.png" alt="alt img">
               <p>定番ゲームから変わり種まで、その数なんと0000種類! 今まで海外カジノでしか楽しめなかった生ディーラーとの真剣勝負が、あなたのスマホにやってきます!</p>
               <button class="primary">
                 <a aria-label="link" :href="data_items.games_brick_section.games_brick_column_1.games_brick_column_1_button_redirect_url">{{ data_items.games_brick_section.games_brick_column_1.games_brick_column_1_button }}</a>
@@ -80,7 +80,7 @@
             </div>
             <div>
               <h3>圧倒的ゲーム数! スロットゲーム</h3>
-              <img src="@/assets/images/vjhp2/ipad.png" alt="alt img">
+              <img width="458" height="458" src="@/assets/images/vjhp2/ipad.png" alt="alt img">
               <p>その数1000種類以上! 人気スロットはもちろん、パチスロみたいな新感覚ゲームまでなんでもあり! 最新作がどこよりも早くプレイできちゃうのも業界ナンバーワンカジノならでは♪</p>
               <button class="error">
                 <a aria-label="link" :href="data_items.games_brick_section.games_brick_column_2.games_brick_column_2_button_redirect_url">{{ data_items.games_brick_section.games_brick_column_2.games_brick_column_2_button }}</a>
@@ -91,7 +91,7 @@
         <section id="section-3">
           <div class="container column-2">
             <div>
-              <img src="@/assets/images/vjhp2/neutral-transparent.png" alt="alt img">
+              <img width="445" height="286" src="@/assets/images/vjhp2/neutral-transparent.png" alt="alt img">
             </div>
             <div>
               <h2>カード・オブ・フロンティア</h2>
@@ -104,17 +104,17 @@
             <div>
               <h3>完全版! ビギナーズガイド</h3>
               <p>オンラインカジノが初めてでも大丈夫! 各種ゲームの探し方から遊び方、入出金方法などを 完全網羅する「ビギナーズガイド」がお手伝いします!</p>
-              <img src="@/assets/images/vjhp2/reward-component_1.png" alt="alt img">
+              <img width="320" height="187" src="@/assets/images/vjhp2/reward-component_1.png" alt="alt img">
             </div>
             <div>
               <h3>ご褒美プログラム</h3>
               <p>楽しく遊んでコインを集めて、ベラジョン・ショッ プでお買い物! フリースピンやスピンクレジット、キャッシュアイ テムとも交換できちゃいます!</p>
-              <img src="@/assets/images/vjhp2/reward-component_2.png" alt="alt img">
+              <img width="320" height="260" src="@/assets/images/vjhp2/reward-component_2.png" alt="alt img">
             </div>
             <div>
               <h3>毎日開催トーナメント</h3>
               <p>勝ち方いろいろ、賞金いろいろ、多種多様なトーナ メントが毎日開催! 得意なゲームでいざ勝負!</p>
-              <img src="@/assets/images/vjhp2/reward-component_3.png" alt="alt img">
+              <img width="320" height="232" src="@/assets/images/vjhp2/reward-component_3.png" alt="alt img">
             </div>
           </div>
         </section>
@@ -135,12 +135,12 @@
             <div>
               <h3>決済方法</h3>
               <p>お好きな方法で入出金がラクラク簡単!</p>
-              <img src="@/assets/images/vjhp2/footerPayment.png" alt="alt img">
+              <img width="324" height="167" src="@/assets/images/vjhp2/footerPayment.png" alt="alt img">
             </div>
             <div>
               <h3>なにかでお困りですか?</h3>
               <p>日本語カスタマーサポートがお手伝いいたします。</p>
-              <img src="@/assets/images/vjhp2/footerSupport.png" alt="alt img">
+              <img width="324" height="326" src="@/assets/images/vjhp2/footerSupport.png" alt="alt img">
             </div>
           </div>
         </section>
@@ -159,10 +159,10 @@
               </div>
               <div>
                 <a aria-label="link" href="https://www.verajohn.com/home">
-                  <img src="@/assets/images/vjhp2/curacao.png" alt="alt img">
+                  <img width="34" height="31" src="@/assets/images/vjhp2/curacao.png" alt="alt img">
                 </a>
                 <a aria-label="link" href="https://www.verajohn.com/home">
-                  <img src="@/assets/images/vjhp2/18Regulator.png" alt="alt img">
+                  <img width="34" height="31" src="@/assets/images/vjhp2/18Regulator.png" alt="alt img">
                 </a>
               </div>
             </div>
