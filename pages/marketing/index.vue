068dcce02bd1e10fa37046e10138580e78c2a261
Nathan Lerit <nathan.lerit@gamesysgroup.com>
2020-12-23 20:28:43 +0800

    update

--- pages/marketing/index.vue
+++ pages/marketing/index.vue
@@ -53,7 +53,7 @@ export default {
   #gradient-wrapper{
     background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
     background-size: 400% 400%;
-    animation: gradient 15s ease infinite;
+    animation: gradient 12s ease infinite;
     position: absolute;
     top: 0;
     left: 0;
@@ -61,7 +61,6 @@ export default {
     height: 100vh;
     z-index: 0;
     mix-blend-mode: soft-light;
-    opacity: .9;
   }
 
   #section-1{
