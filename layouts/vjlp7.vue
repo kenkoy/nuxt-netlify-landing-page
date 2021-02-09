2861f4ed1935d8938f52e2508441c1ca8a532bc2
Nathan Lerit <nathan.lerit@gamesysgroup.com>
2021-02-09 13:57:06 +0800

    update vjlp7

--- layouts/vjlp7.vue
+++ layouts/vjlp7.vue
@@ -7,7 +7,7 @@
       :class="mdData.styles"
     >
       <section id="hero" :style="cssBackground">
-        <div class="container" style="height: calc(100vh - 150px)">
+        <div class="container">
           <div id="hero-text-wrapper">
             <div>
               <div id="login" class="animated">
