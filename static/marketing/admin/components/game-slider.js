ef6f9fc87964d304f0cf058ab6567d43b0d4f7c9
Mat Francisco <mat.francisco@gamesysgroup.com>
2021-02-17 14:59:34 +0800

    Implemented image slider

--- static/marketing/admin/components/game-slider.js
+++ static/marketing/admin/components/game-slider.js
@@ -4,19 +4,31 @@ import htm from 'https://unpkg.com/htm?module'
 const html = htm.bind(h)
 export const GAME_SLIDER = createClass({
   render () {
-    // const { games } = this.props
+    const { games } = this.props
 
     const styles = {
       gameSlider: {
-        boxShadow: '0px 5px 5px 0px rgba(0, 0, 0, 0.2)',
+        'box-shadow': '0px 5px 5px 0px rgba(0,0,0,0.2)',
         padding: '10px 0'
       },
-      container: { display: 'block' }
+      container: {
+        display: 'block',
+        padding: '0 10%'
+      },
+      slider: {
+        display: 'flex',
+        'justify-content': 'space-evenly'
+      }
     }
 
     return (html`
-    <section style=${styles.gameSlider}>
+    <section id="game-slider" style=${styles.gameSlider}>
       <div style=${styles.container}>
+        <div style=${styles.slider}>
+          ${games.map(game =>
+            html`<img src="${game.icon}"></img>`
+          )}
+        </div>
       </div>
     </section>
     `)
