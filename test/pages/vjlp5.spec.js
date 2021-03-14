278eee3417d206e23a6998e8504888f067e7f8cd
Nathan Lerit <nathan.lerit@gamesysgroup.com>
2021-03-14 13:44:41 +0800

    vjlp5 unit test

--- test/pages/vjlp5.spec.js
+++ test/pages/vjlp5.spec.js
@@ -13,49 +13,46 @@ describe('Testing VJLP5 index.vue', () => {
     const localVue = createLocalVue()
     localVue.use(VueMeta, { keyName: 'head' })
 
-    //For MD Files
+    //==== For MD Files ====
     const md = _
       .chain(await retrieveFiles(VJLP5_DIR, '.md'))
       .map(files => VJLP5_DIR + files)
       .value()
-    let dataMD = []
+    let dataMDContent = []
     let jsonData = []
 
     md.forEach((item, i) => {
       jsonData.push(retriveFrontMattertoJSON(item))
       jsonData.forEach((attr, i) => {
-        dataMD.push(attr.attributes)
+        dataMDContent.push(attr.attributes)
       })
     })
-    datum = dataMD
+    datum = dataMDContent
 
 
-    //For Vue Components
-    // const mdComponent = _
-    //   .chain(await retrieveFiles(VJLP5_DIR, '.md'))
-    //   .map(files => VJLP5_DIR + files)
-    //   .sample()
-    //   .value()
-    // const dataMDComponent = retriveFrontMattertoJSON(mdComponent)
-    //
-    // wrapper = mount(index, {
-    //   localVue,
-    //   data () {
-    //     return {
-    //       dataMDComponent
-    //     }
-    //   }
-    // })
-    // metaInfo = wrapper.vm.$metaInfo
-    // tagIds = dataMD.attributes.field_ids
-    // locale = dataMD.attributes.promo_locale
+    //==== For Vue Components ====
+    const mdComponents = _
+      .chain(await retrieveFiles(VJLP5_DIR, '.md'))
+      .map(files => VJLP5_DIR + files)
+      .sample()
+      .value()
+
+    const dataMD = retriveFrontMattertoJSON(mdComponents)
+    wrapper = mount(index, {
+      localVue,
+      data () {
+        return {
+          dataMD
+        }
+      }
+    })
+    metaInfo = wrapper.vm.$metaInfo
+    tagIds = dataMD.attributes.field_ids
+    locale = dataMD.attributes.promo_locale
   })
 
-  // test('Title should be Vera&John', () => {
-  //   const pageTitle = metaInfo.title
-  //   expect(pageTitle).toBe('Vera&John')
-  // })
 
+  //====  MD FILES TEST ====
   test('Banner should be first_title', () => {
     const title = datum
     title.forEach((item, i) => {
@@ -63,50 +60,57 @@ describe('Testing VJLP5 index.vue', () => {
       expect(banner_title).toBe(`${item.promo_banner.first_title}`)
     })
   })
-  //
-  // test('HTML language attribute should not be null or undefined', () => {
-  //   const languageAttribute = metaInfo.htmlAttrs.lang
-  //   expect(languageAttribute).not.toBeNull()
-  //   expect(languageAttribute).not.toHaveLength(0)
-  // })
-  //
-  // test('Body ID attriute should be LANGUAGE CODE-COUNTRY_CODE', () => {
-  //   const countryCode = locale.promo_country_code
-  //   const languageCode = locale.promo_language_code
-  //   expect(countryCode).not.toBeNull()
-  //   expect(countryCode).not.toHaveLength(0)
-  //
-  //   const idAttribute = metaInfo.bodyAttrs.id
-  //   expect(idAttribute).toBe(`${languageCode}-${countryCode}`)
-  // })
-  //
-  // test('Google Optimize ID and scripts should be present', () => {
-  //   const googleOptimizeId = tagIds.go_container_id
-  //   expect(googleOptimizeId).not.toBeNull()
-  //   expect(googleOptimizeId).not.toHaveLength(0)
-  //
-  //   const googleOptimizeScript = metaInfo.script.find(s => s.hid === 'goHead').innerHTML
-  //   expect(googleOptimizeScript.includes(googleOptimizeId)).toBeTruthy()
-  // })
-  //
-  // test('Google Analytics ID and scripts should be present', () => {
-  //   const googleAnalyticsId = tagIds.ga_tracking_id
-  //   expect(googleAnalyticsId).not.toBeNull()
-  //   expect(googleAnalyticsId).not.toHaveLength(0)
-  //
-  //   const googleAnalyticsScript = metaInfo.script.find(s => s.hid === 'gaHead').innerHTML
-  //   expect(googleAnalyticsScript.includes(googleAnalyticsId)).toBeTruthy()
-  // })
-  //
-  // test('Google Tag Mananger ID and scripts should be present', () => {
-  //   const googleTagMangerId = tagIds.gtm_container_id
-  //   expect(googleTagMangerId).not.toBeNull()
-  //   expect(googleTagMangerId).not.toHaveLength(0)
-  //
-  //   const googleTagManagerScript = metaInfo.script.find(s => s.hid === 'gtmHead').innerHTML
-  //   expect(googleTagManagerScript.includes(googleTagMangerId)).toBeTruthy()
-  //
-  //   const googleTagManagerNoScript = metaInfo.noscript.find(s => s.hid === 'gtmBody').innerHTML
-  //   expect(googleTagManagerNoScript.includes(googleTagMangerId)).toBeTruthy()
-  // })
+
+
+  //==== COMPONENTS TEST ====
+  test('Title should be Vera&John', () => {
+    const pageTitle = metaInfo.title
+    expect(pageTitle).toBe('Vera&John')
+  })
+
+  test('HTML language attribute should not be null or undefined', () => {
+    const languageAttribute = metaInfo.htmlAttrs.lang
+    expect(languageAttribute).not.toBeNull()
+    expect(languageAttribute).not.toHaveLength(0)
+  })
+
+  test('Body ID attriute should be LANGUAGE CODE-COUNTRY_CODE', () => {
+    const countryCode = locale.promo_country_code
+    const languageCode = locale.promo_language_code
+    expect(countryCode).not.toBeNull()
+    expect(countryCode).not.toHaveLength(0)
+
+    const idAttribute = metaInfo.bodyAttrs.id
+    expect(idAttribute).toBe(`${languageCode}-${countryCode}`)
+  })
+
+  test('Google Optimize ID and scripts should be present', () => {
+    const googleOptimizeId = tagIds.go_container_id
+    expect(googleOptimizeId).not.toBeNull()
+    expect(googleOptimizeId).not.toHaveLength(0)
+
+    const googleOptimizeScript = metaInfo.script.find(s => s.hid === 'goHead').innerHTML
+    expect(googleOptimizeScript.includes(googleOptimizeId)).toBeTruthy()
+  })
+
+  test('Google Analytics ID and scripts should be present', () => {
+    const googleAnalyticsId = tagIds.ga_tracking_id
+    expect(googleAnalyticsId).not.toBeNull()
+    expect(googleAnalyticsId).not.toHaveLength(0)
+
+    const googleAnalyticsScript = metaInfo.script.find(s => s.hid === 'gaHead').innerHTML
+    expect(googleAnalyticsScript.includes(googleAnalyticsId)).toBeTruthy()
+  })
+
+  test('Google Tag Mananger ID and scripts should be present', () => {
+    const googleTagMangerId = tagIds.gtm_container_id
+    expect(googleTagMangerId).not.toBeNull()
+    expect(googleTagMangerId).not.toHaveLength(0)
+
+    const googleTagManagerScript = metaInfo.script.find(s => s.hid === 'gtmHead').innerHTML
+    expect(googleTagManagerScript.includes(googleTagMangerId)).toBeTruthy()
+
+    const googleTagManagerNoScript = metaInfo.noscript.find(s => s.hid === 'gtmBody').innerHTML
+    expect(googleTagManagerNoScript.includes(googleTagMangerId)).toBeTruthy()
+  })
 })
