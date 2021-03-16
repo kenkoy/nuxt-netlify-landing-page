b6f6e8f67c03e1f0223d9718bb36738e61032145
Nathan Lerit <nathan.lerit@gamesysgroup.com>
2021-03-16 13:55:41 +0800

    update vjlp5 specs

--- test/pages/vjlp5.spec.js
+++ test/pages/vjlp5.spec.js
@@ -1,7 +1,3 @@
-// RUN
-// npm run test -- --silent=false
-// to see console error
-
 import { mount, createLocalVue } from '@vue/test-utils'
 import VueMeta from 'vue-meta'
 import _ from 'lodash'
@@ -58,84 +54,59 @@ describe('Testing VJLP5 index.vue', () => {
   //  HERO SECTION
   test('Banner title should be first_title', () => {
     const title = datum
+    let error = []
+
+    //Loop for printing specific MD files with error
     title.forEach((item, i) => {
-      const bannerTitle = item.promo_banner.first_title
-      if (bannerTitle !== `${item.promo_banner.first_title}`) {
-        console.log('ERROR FILE', item.slug_name)
+      if (item.promo_banner.first_title === undefined) {
+        error.push(item.slug_name)
+        let unique = []
+
+        error.forEach((element, i) => {
+          if (!unique.includes(element)) {
+            unique.push(element)
+          }
+        })
+        error = unique
       }
-      expect(bannerTitle).toBe(`${item.promo_banner.first_title}`)
     })
-  })
-  test('Banner subtitle should be second_title', () => {
-    const subtitle = datum
-    subtitle.forEach((item, i) => {
-      const bannerSubtitle = item.promo_banner.second_title
-      if (bannerSubtitle !== `${item.promo_banner.second_title}`) {
-        console.log('ERROR FILE', item.slug_name)
-      }
-      expect(bannerSubtitle).toBe(`${item.promo_banner.second_title}`)
-    })
-  })
-  //  STEPS / CAROUSEL
-  test('Steps title shoud be step_title_1', () => {
-    const steps = datum
-    steps.forEach((item, i) => {
-      const titleOne = item.steps.step_title_1
-      if (titleOne !== `${item.steps.step_title_1}`) {
-        console.log('ERROR FILE', item.slug_name)
-      }
-      expect(titleOne).toBe(`${item.steps.step_title_1}`)
-    })
-  })
-  test('Steps title shoud be step_title_2', () => {
-    const steps = datum
-    steps.forEach((item, i) => {
-      const titleTwo = item.steps.step_title_2
-      if (titleTwo !== `${item.steps.step_title_2}`) {
-        console.log('ERROR FILE', item.slug_name)
-      }
-      expect(titleTwo).toBe(`${item.steps.step_title_2}`)
-    })
-  })
-  test('Steps title shoud be step_title_3', () => {
-    const steps = datum
-    steps.forEach((item, i) => {
-      const titleThree = item.steps.step_title_3
-      if (titleThree !== `${item.steps.step_title_3}`) {
-        console.log('ERROR FILE', item.slug_name)
-      }
-      expect(titleThree).toBe(`${item.steps.step_title_3}`)
+    if(error.length > 0){
+      console.log('Error files: ', error.join("\r\n"))
+    }
+
+    //Loop for testing the actual MD files
+    title.forEach((output, i) => {
+      const bannerTitle = output.promo_banner.first_title
+      expect(bannerTitle).toBe(`${output.promo_banner.first_title}`)
     })
   })
 
-  test('Steps description shoud be step_description_1', () => {
-    const steps = datum
-    steps.forEach((item, i) => {
-      const descOne = item.steps.step_description_1
-      if (descOne !== `${item.steps.step_description_1}`) {
-        console.log('ERROR FILE', item.slug_name)
+  test('Banner title should be second_title', () => {
+    const subTitle = datum
+    let error = []
+
+    //Loop for printing specific MD files with error
+    subTitle.forEach((item, i) => {
+      if (item.promo_banner.second_title === undefined) {
+        error.push(item.slug_name)
+        let unique = []
+
+        error.forEach((element, i) => {
+          if (!unique.includes(element)) {
+            unique.push(element)
+          }
+        })
+        error = unique
       }
-      expect(descOne).toBe(`${item.steps.step_description_1}`)
     })
-  })
-  test('Steps description shoud be step_description_2', () => {
-    const steps = datum
-    steps.forEach((item, i) => {
-      const descTwo = item.steps.step_description_2
-      if (descTwo !== `${item.steps.step_description_2}`) {
-        console.log('ERROR FILE', item.slug_name)
-      }
-      expect(descTwo).toBe(`${item.steps.step_description_2}`)
-    })
-  })
-  test('Steps description shoud be step_description_3', () => {
-    const steps = datum
-    steps.forEach((item, i) => {
-      const descThree = item.steps.step_description_3
-      if (descThree !== `${item.steps.step_description_3}`) {
-        console.log('ERROR FILE', item.slug_name)
-      }
-      expect(descThree).toBe(`${item.steps.step_description_3}`)
+    if(error.length > 0){
+      console.log('Error files: ', error.join("\r\n"))
+    }
+
+    //Loop for testing the actual MD files
+    subTitle.forEach((output, i) => {
+      const bannerTitle = output.promo_banner.second_title
+      expect(bannerTitle).toBe(`${output.promo_banner.second_title}`)
     })
   })
 
