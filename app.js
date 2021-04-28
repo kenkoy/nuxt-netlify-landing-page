02252f5cfc5560f60b5aa9d62260ea43ec536b22
Mat Francisco <mat.francisco@gamesysgroup.com>
2021-04-28 16:54:56 +0800

    Changed fs/promies to fs

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require('fs/promises')
+const fs = require('fs')
 const axios = require('axios')
 
 const gameDataPath = './assets/content/games.json'
@@ -6,7 +6,7 @@ const gameDataPath = './assets/content/games.json'
 axios.get('https://www.verajohn.com/ajax/load/data/games/INTERNAL/ALL_GAMES/default/asc/all')
   .then((res) => {
     const jsonData = JSON.stringify(res.data)
-    fs.writeFile(gameDataPath, jsonData, 'utf-8')
+    fs.promises.writeFile(gameDataPath, jsonData, 'utf-8')
   })
   .then((res) => {
     // eslint-disable-next-line no-console
