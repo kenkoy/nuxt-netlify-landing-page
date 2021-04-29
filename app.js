087b4a19f9d07babe3ddd799360136ec0cc7ea7f
Mat Francisco <mat.francisco@gamesysgroup.com>
2021-04-29 10:19:33 +0800

    Changed the implementation of the data retrieval script

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-import fs from 'fs/promises'
-import axios from 'axios'
+const fs = require('fs')
+const axios = require('axios')
 
 const gameDataPath = './assets/content/games.json'
 
 axios.get('https://www.verajohn.com/ajax/load/data/games/INTERNAL/ALL_GAMES/default/asc/all')
   .then((res) => {
     const jsonData = JSON.stringify(res.data)
-    fs.writeFile(gameDataPath, jsonData, 'utf-8')
+    fs.promises.writeFile(gameDataPath, jsonData, 'utf-8')
   })
   .then((res) => {
     // eslint-disable-next-line no-console
