12df7a6f076f5592475152e76f522e1f5e3743d5
Nathan Lerit <nathan.lerit@gamesysgroup.com>
2021-04-29 08:51:36 +0800

    update

--- pages/marketing/games/_slug/index.vue
+++ pages/marketing/games/_slug/index.vue
@@ -3,34 +3,34 @@
 </template>
 
 <script>
-import Game from '@/models/Game'
+// import Game from '@/models/Game'
 
 export default {
   layout: 'games',
-  async asyncData ({ params, error }) {
-    try {
-      const game = await import('~/assets/content/games.json')
-      const gameData = game.data
-      return {
-        gameFilterData: {
-          filter: gameData.available_filters.GAME_FINDER.map(f => ({
-            id: f.filterid,
-            name: f.filtername,
-            code: f.filtercode,
-            description: f.description
-          })),
-          sort: gameData.available_sort,
-          provider: [],
-          games: gameData.feed.map((f) => {
-            return new Game(f.game_title, f.game_name_url, f.game_image)
-          })
-        },
-        skin: params.slug
-      }
-    } catch (e) {
-      error(e)
-    }
-  },
+  // async asyncData ({ params, error }) {
+  //   try {
+  //     const game = await import('~/assets/content/games.json')
+  //     const gameData = game.data
+  //     return {
+  //       gameFilterData: {
+  //         filter: gameData.available_filters.GAME_FINDER.map(f => ({
+  //           id: f.filterid,
+  //           name: f.filtername,
+  //           code: f.filtercode,
+  //           description: f.description
+  //         })),
+  //         sort: gameData.available_sort,
+  //         provider: [],
+  //         games: gameData.feed.map((f) => {
+  //           return new Game(f.game_title, f.game_name_url, f.game_image)
+  //         })
+  //       },
+  //       skin: params.slug
+  //     }
+  //   } catch (e) {
+  //     error(e)
+  //   }
+  // },
   mounted () {
     this.emitData(this.gameFilterData, this.skin)
   },
