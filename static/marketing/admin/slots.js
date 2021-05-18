fb04cbfa69ecbdb462a2510ba973194191566200
Danilo Panahon <danpan@jpj.com>
2021-05-18 17:05:11 +0800

    Slots Preview

---
+++ static/marketing/admin/slots.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+import htm from './external-scripts/htm.module.js'
+
+const html = htm.bind(h)
+export const SLOTS_PREVIEW = createClass({
+  componentDidMount () {
+    const { document } = this.props
+    const link = document.createElement('link')
+    link.rel = 'stylesheet'
+    link.type = 'text/css'
+    link.href = '/marketing/styles/slots/previewPane.css'
+    document.head.appendChild(link)
+  },
+  render () {
+    return (html`
+    <body>
+    <h1>Hello World!</h1>
+  </body>
+    `)
+  }
+})
