2607c9214a543303bf83ac9b07ca4e2eaf975f73
Nathan Lerit <nathan.lerit@gamesysgroup.com>
2021-05-20 19:10:54 +0800

    added accordion components

--- layouts/lobby-pages/index.vue
+++ layouts/lobby-pages/index.vue
@@ -6,13 +6,13 @@
       <main :class="pageSlide">
         <!-- Display when Sidemenu is visible -->
         <div v-if="menuToggle" id="overlay" />
-        <Header :class="menuToggle ? 'nav-slide-up' : 'nav-slide-down'" @page-slide-on="pageSlideOn" />
+        <Header :class="menuToggle && headerScrollHeight ? 'nav-slide-up' : 'nav-slide-down'" @page-slide-on="pageSlideOn" />
 
         <!-- PAGES -->
         <Lobbylayout v-if="mdData.template === 'lobby'" />
         <Tournamentlayout v-if="mdData.template === 'tournament'" />
       </main>
-      <FooterFixButtons :class="menuToggle ? 'footer-slide-down' : 'footer-slide-up'" />
+      <FooterFixButtons :class="menuToggle ? 'footer-slide-down' : 'footer-slide-up'" /> <!-- add 'scrollBottom' to slide down when the page reach the bottom -->
     </div>
   </div>
 </template>
@@ -34,9 +34,10 @@ export default {
   data () {
     return {
       mdData: {},
-      menuToggle: false,
-      pageSlide: ''
-      // scrollBottom: false
+      menuToggle: false, // If true, SideMenu, FooterFixButtons and Main content will animate/slide
+      headerScrollHeight: false, // If true (scroll more than 54px), the header-menu will disappear when SideMenu is open
+      pageSlide: '',
+      scrollBottom: false // If true (scroll to the bottom page), footer buttons will disappear
     }
   },
   created () {
@@ -62,14 +63,17 @@ export default {
       setTimeout(() => {
         this.menuToggle = false
       }, 300)
+    },
+    handleScroll (event) {
+      if (document.getElementById('header-menu').offsetHeight > 54) {
+        this.headerScrollHeight = true
+      }
+      if (document.documentElement.scrollTop + window.innerHeight === document.documentElement.scrollHeight) {
+        this.scrollBottom = true
+      } else if (document.documentElement.scrollTop + window.innerHeight < document.documentElement.scrollHeight - document.getElementById('footer-icon-wrapper').offsetHeight) {
+        this.scrollBottom = false
+      }
     }
-    // handleScroll (event) {
-    //   if (document.documentElement.scrollTop + window.innerHeight === document.documentElement.scrollHeight) {
-    //     this.scrollBottom = true
-    //   } else if (document.documentElement.scrollTop + window.innerHeight < document.documentElement.scrollHeight - document.getElementById('footer-icon-wrapper').offsetHeight) {
-    //     this.scrollBottom = false
-    //   }
-    // }
   }
 }
 </script>
--- layouts/lobby-pages/tournament.vue
+++ layouts/lobby-pages/tournament.vue
@@ -13,18 +13,11 @@
 
       <section>
         <div class="container">
-          <div class="row">
-            <div class="col">
-              <div class="tabs">
-                <div v-for="(accordionData, accordionData_index) in mdData.accordion" :key="accordionData_index" class="tab">
-                  <input :id="accordionData_index" :type="openMultiAccordion ? 'checkbox' : 'radio'" :name="openMultiAccordion ? '' : 'accordion'">
-                  <label class="tab-label" :for="accordionData_index">{{ accordionData.title }}</label>
-                  <div class="tab-content">
-                    {{ accordionData.description }}
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div class="row negate-gutter">
+            <Accordion
+              :accordion-data="mdData.accordion"
+              :open-multi-accordion="true"
+            />
           </div>
         </div>
       </section>
@@ -85,12 +78,15 @@
 </template>
 
 <script>
+import Accordion from '~/components/Lobby-Pages/Accordion.vue'
+
 export default {
+  components: {
+    Accordion
+  },
   data () {
     return {
-      mdData: {},
-      openMultiAccordion: true
-      // scrollBottom: false
+      mdData: {}
     }
   },
   created () {
@@ -103,75 +99,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss">
-$midnight: #2c3e50;
-input {
-  position: absolute;
-  opacity: 0;
-  z-index: -1;
-}
-/* Accordion styles */
-.tabs {
-  border-radius: 8px;
-  overflow: hidden;
-  box-shadow: 0 4px 4px -2px rgba(0,0,0,0.5);
-}
-.tab {
-  width: 100%;
-  color: white;
-  overflow: hidden;
-  &-label {
-    display: flex;
-    justify-content: space-between;
-    padding: 1em;
-    background: $midnight;
-    font-weight: bold;
-    cursor: pointer;
-    /* Icon */
-    &:hover {
-      background: darken($midnight, 10%);
-    }
-    &::after {
-      content: "\276F";
-      width: 1em;
-      height: 1em;
-      text-align: center;
-      transition: all .35s;
-    }
-  }
-  &-content {
-    max-height: 0;
-    padding: 0 1em;
-    color: $midnight;
-    background: white;
-    transition: all .35s;
-  }
-  &-close {
-    display: flex;
-    justify-content: flex-end;
-    padding: 1em;
-    font-size: 0.75em;
-    background: $midnight;
-    cursor: pointer;
-    &:hover {
-      background: darken($midnight, 10%);
-    }
-  }
-}
-
-// :checked
-input:checked {
-  + .tab-label {
-    background: darken($midnight, 10%);
-    &::after {
-      transform: rotate(90deg);
-    }
-  }
-  ~ .tab-content {
-    max-height: 100vh;
-    padding: 1em;
-  }
-}
-
-</style>
