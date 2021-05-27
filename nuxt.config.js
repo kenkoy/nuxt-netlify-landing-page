22d4f4a877d7f08b95e90bf0ee6c46a3cfe692c1
Mat Francisco <mat.francisco@gamesysgroup.com>
2021-05-27 15:19:32 +0800

    Disabled some of the globe meta properties

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,9 +58,9 @@ export default {
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
-      { hid: 'robots', name: 'robots', content: 'noindex' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      // { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      // { hid: 'robots', name: 'robots', content: 'noindex' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/marketing/gamesys-favicon.ico' }
