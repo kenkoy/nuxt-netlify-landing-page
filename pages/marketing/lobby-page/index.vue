7a8c3c5a5c889719a00d09a814df50df161d0644
Mat Francisco <mat.francisco@gamesysgroup.com>
2021-05-27 11:34:48 +0800

    Testing lobby-page route to have links

--- pages/marketing/lobby-page/index.vue
+++ pages/marketing/lobby-page/index.vue
@@ -1,13 +1,36 @@
 <template>
-  <div />
+  <div class="post-page">
+    <div v-for="(post, postIndex) in posts" :key="postIndex" class="posts">
+      <nuxt-link :to="'/marketing/' + post.path + '/' + post.slug" class="post-preview">
+        <article>
+          <h3>{{ post.attributes.slug_name }}</h3>
+        </article>
+      </nuxt-link>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
 
+export default {
+  layout: 'default',
+  async asyncData () {
+    const mdFiles = await require.context(
+      '~/assets/content/landing-page/marketing/lobby-page/',
+      true,
+      /\.md$/
+    )
+    // add slug and path in key list of mdFiles
+    const mdFilesContent = await mdFiles.keys().map(key => ({
+      ...mdFiles(key),
+      slug: `${key.replace('.md', '').replace('./', '')}`,
+      path: 'lobby-page'
+    }))
+    // filter to vjlp3 templates only
+    // .filter((mdFiles) => mdFiles.attributes.promo_template == 'VJLP3-NoForm')
+
+    // return { posts: mdFilesContent.reverse(), dataMD: mdFilesContent.reverse() }
+    return { posts: mdFilesContent.reverse() }
+  }
 }
 </script>
-
-<style>
-
-</style>
