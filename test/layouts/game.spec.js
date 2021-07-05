1685d010e98fcfc0447edc2aa081d13002186993
Nathan Lerit <nathan.lerit@gamesysgroup.com>
2021-07-05 20:00:01 +0800

    update jest

--- test/layouts/game.spec.js
+++ test/layouts/game.spec.js
@@ -1,4 +1,4 @@
-import { shallowMount, createLocalVue, createWrapper, mount, someFunctionReturningHTMLString } from '@vue/test-utils'
+import { createLocalVue, mount, nuxt } from '@vue/test-utils'
 import VueMeta from 'vue-meta'
 import _ from 'lodash'
 import layout from '@/layouts/game-page/game-layout.vue'
@@ -8,7 +8,7 @@ import {jest} from '@jest/globals'
 const GAME_DIR = '/assets/content/game-page/'
 
 describe('Testing Game index.vue', () => {
-  let mdData, markDownData, wrapper, rootWrapper, error, localVue, dataMD
+  let mdData, wrapper, localVue, dataMD
 
   beforeAll(async () => {
     const mdFiles = _
@@ -26,21 +26,22 @@ describe('Testing Game index.vue', () => {
       .value()
 
     dataMD = retriveFrontMattertoJSON(randomMarkdown)
+
+    wrapper = mount (layout)
   })
   test('layout-css CLASS should exist', () => {
-    const wrapper = mount(layout)
-    const layoutWrapperClass = wrapper.vm.$el._prevClass
-    expect(layoutWrapperClass).toContain('layout-css')
+    if (dataMD.length > 0){
+      const layoutCss = wrapper.findAll('.layout-css')
+      expect(layoutCss.length).toBe(1)
+    }
   }),
   test('Iframe should exist', () => {
-    const wrapper = mount(layout)
     if (dataMD.length > 0){
       const iframe = wrapper.findAll('iframe')
       expect(iframe.length).toBe(1)
     }
   }),
   test('Sticky footer ID name should be sticky-footer', () => {
-    const wrapper = mount(layout)
     if (dataMD.length > 0){
       const byId = wrapper.find('#sticky-footer')
       expect(byId.element.id).toBe('sticky-footer')
