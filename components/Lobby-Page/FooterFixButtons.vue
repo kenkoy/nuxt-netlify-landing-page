0f6ddaf529e428d6779f44075e978617d78f9db7
Allaine Adriano-Calumba <alladr@jackpotjoygroup.com>
2021-07-16 12:17:33 +0800

    Added URL links for home, sign in and join now buttons.

--- components/Lobby-Page/FooterFixButtons.vue
+++ components/Lobby-Page/FooterFixButtons.vue
@@ -1,12 +1,12 @@
 <template>
   <aside id="footer-login-register">
     <button class="n-button n-button--secondary">
-      <a class="btn-login" :href="brand === 'verajohn' ? 'https://verajohn.com/#signin' : ''">
+      <a class="btn-login" :href="brand === 'intercasino' ? 'https://intercasino.com/#signin' : 'https://verajohn.com/#signin'">
         ログイン
       </a>
     </button>
     <button class="n-button n-button--primary">
-      <a class="btn-register" :href="brand === 'verajohn' ? 'https://verajohn.com/#join' : ''">
+      <a class="btn-register" :href="brand === 'intercasino' ? 'https://intercasino.com/#join' : 'https://verajohn.com/#join'">
         今すぐ登録しよう!
       </a>
     </button>
--- components/Lobby-Page/Header.vue
+++ components/Lobby-Page/Header.vue
@@ -1,20 +1,20 @@
 <template>
   <header id="header-menu">
     <div id="header-home">
-      <a :href="brand === 'verajohn' ? 'https://verajohn.com/ja' : ''">
+      <a :href="brand === 'intercasino' ? 'https://intercasino.com/ja' : 'https://verajohn.com/ja'">
         <img :src="require(`@/assets/images/lobby-page/${brand}/home-icon.png`)" alt="alt img">
       </a>
     </div>
 
     <div id="header-logo">
-      <a :href="brand === 'verajohn' ? 'https://verajohn.com/ja' : ''">
+      <a :href="brand === 'intercasino' ? 'https://intercasino.com/ja' : 'https://verajohn.com/ja'">
         <img class="logo" :src="require(`@/assets/images/lobby-page/${brand}/logo.png`)" alt="alt img">
       </a>
     </div>
 
     <div id="header-button">
       <button class="cta_button n-button--secondary">
-        <a class="btn-login" :href="brand === 'verajohn' ? 'https://verajohn.com/#signin' : ''">
+        <a class="btn-login" :href="brand === 'intercasino' ? 'https://intercasino.com/#signin' : 'https://verajohn.com/#signin'">
           ログイン
         </a>
       </button>
